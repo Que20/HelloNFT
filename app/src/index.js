de7b9e961cf72c60b06358d59ae1b63c96b408f4
Kevin Maarek <kmaarek@freebox.fr>
2021-03-11 20:55:53 +0100

    Removed unused files + code cleaning.

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css';
-import App from './views/app/App';
-import LandingPage from './views/landing-page/LandingPage';
-import reportWebVitals from './reportWebVitals';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+import App from './views/app/App'
+import LandingPage from './views/landing-page/LandingPage'
+import reportWebVitals from './reportWebVitals'
 import {
     BrowserRouter as Router,
     Switch,
@@ -38,4 +37,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
