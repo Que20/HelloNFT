87ad70299755f79d1192c3e61cfb4abd9cccc73a
Kevin Maarek <kmaarek@freebox.fr>
2021-03-15 15:53:48 +0100

    Implemented API calls in the API abstraction class.

--- app/src/helpers/api.js
+++ app/src/helpers/api.js
@@ -4,15 +4,21 @@ class API {
     constructor() {}
     
 	registerNewToken(tokenId, address, callback) {
-		callback(true)
+		this._post('http://localhost:8888/api/token', { token_id: tokenId, address: address }, (res, err) => {
+            callback(res != null)
+        }) 
 	}
 
     getNickname(address, callback) {
-        //
+        this._get('http://localhost:8888/api/address/'+address, (res, err) => {
+            callback(res.nickname)
+        })
     }
 
     getToken(tokenId, callback) {
-        //
+        this._get('http://localhost:8888/api/token/'+tokenId, (res, err) => {
+            callback(res)
+        })
     }
 
     _post(url, data, callback) {
