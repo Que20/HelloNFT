44353c9814d7339b7204c313cc4f73ead5cbc87c
Kevin Maarek <kmaarek@freebox.fr>
2021-03-15 15:56:21 +0100

    Refactored home view, removed table + added import button and added call for transfert modal.

--- app/src/views/app/home/Home.js
+++ app/src/views/app/home/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
+import API from '../../../helpers/api'
+import TransfertModal from './TransfertModal'
+import { generateFromString } from 'generate-avatar'
 import { Button } from 'semantic-ui-react'
 
 class TokenList extends Component {
 	constructor(props) {
 		super(props)
+        this.api = new API()
         this.state = {
             tokenIdList: [],
             tokenList: [],
+            modalShown: false,
+            transfertTokenData: null
         }
 	}
 
@@ -25,7 +31,6 @@ class TokenList extends Component {
                         tmp.push(tokenFull)
                         if (this.state.tokenIdList.length == tmp.length) {
                             this.setState({ tokenList: tmp})
-                            console.log(this.state)
                         }
                     })
                 }
@@ -34,12 +39,8 @@ class TokenList extends Component {
     }
 
     getTokenData(id, callback) {
-        axios.get('http://localhost:8888/api/token/'+id)
-        .then((response) => {
+        this.api.getToken(id, (response) => {
             callback(response.data)
-        })
-        .catch(function (error) {
-            console.log(error)
         })
     }
 
@@ -47,40 +48,58 @@ class TokenList extends Component {
 		return (
             <div>
                 <h1 className="ui header">Your balance of HELLO Token is {this.state.tokenList.length}</h1>
-                <table class="ui celled table">
-                <thead>
-                    <tr>
-                        <th>Nickname</th>
-                        <th>Token Id</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.tokenList.map((token, key) => {
-                            return (
-                                <tr>
-                                    <td data-label="Nickname">{token.data.name}</td>
-                                    <td data-label="Token Id">{token.tokenId}</td>
-                                    <td data-label="Action">
-                                        <div class="ui basic icon buttons">
-                                            <button class="ui button">
-                                                Transfert
-                                            </button>
-                                            <button class="ui button">
-                                                <i class="comments icon"></i>
-                                            </button>
+                <div style={{display: this.state.modalShown == true ? 'block' : 'none'}}>
+                    <TransfertModal eth={this.props.eth} transfertTokenData={this.state.transfertTokenData} onClose={() => {
+                        this.setState({modalShown: false})
+                    }}/>
+                </div>
+                <div className='ui equal width center aligned padded grid'>
+                <div className='column token_list left aligned'>
+                {
+                    this.state.tokenList.map((token, key) => {
+                        let nick = token.data.name
+                        let address = token.data.description
+                        let id = token.tokenId
+                        return (
+                            <div class="ui comments">
+                                <div class="comment">
+                                    <a class="avatar" href="#">
+                                        <img src={`data:image/svg+xml;utf8,${generateFromString(address)}`} alt=""/>
+                                    </a>
+                                    <div class="content">
+                                        <a class="author">{nick}</a>
+                                        <div class="text">
+                                            Token id : {id}
+                                            <br />
+                                            Address : {address}
                                         </div>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-                </table>
+                                    </div>
+                                    <Button onClick={() => {
+                                        this.transfert(id, nick, address) 
+                                    }}>Transfert</Button>
+                                </div>
+                                <div className='separator'></div>
+                            </div>
+                        )
+                    })
+                }
+                </div>
+                <div className='column conversation_box left aligned'>
+                    
+                </div>
+                </div>
             </div>
-		);
+		)
 	}
+
+    transfert = (id, nick, address) => {
+        this.setState({ modalShown: true })
+        this.setState({ transfertTokenData: {
+            id: id,
+            nick: nick,
+            address: address
+        } })
+    }
 }
 
 export default TokenList;
