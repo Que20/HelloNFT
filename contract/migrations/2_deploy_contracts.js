1363dd7baaf67bd6106f105560a7846a92c1bc6e
Kevin Maarek <kmaarek@freebox.fr>
2021-03-15 15:56:48 +0100

    Changed smart contract deployment address.

--- contract/migrations/2_deploy_contracts.js
+++ contract/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Hello = artifacts.require("Hello");
 
 module.exports = function(deployer) {
-  deployer.deploy(Hello , '0xDF6F6688C63347FCb95e7511cfD1F09082B4fAA4');
+  deployer.deploy(Hello , '0x67c76F852453c2924D7Bb63C075627C9055c0Bba');
 };
